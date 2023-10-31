ae1a462a07b6957b5fe6e7347799c690ed37b297
Sausainis777 <a.tankeliun@gmail.com>
2023-10-31 10:50:09 +0200

    Added a new test addInvalidCategory

--- BookRezervationTestAndžej/BookRezervationTest/src/test/java/org/example/CategoriesTest.java
+++ BookRezervationTestAndžej/BookRezervationTest/src/test/java/org/example/CategoriesTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CategoriesTest extends BaseTestPage {
 
-    String categoryName = "History";
+    String categoryName = "history";
     @Test
     void addCategories() {
         MainPage mainPage = new MainPage(driver);
@@ -34,5 +34,16 @@ public class CategoriesTest extends BaseTestPage {
         categoriesPage.clickAddCategory();
         categoriesPage.clickSelectCategory();
         assertTrue(driver.findElement(By.cssSelector("select[name='category']")).getText().contains(categoryName));
+    }
+    @Test
+    void addInvalidCategory(){
+        MainPage mainPage = new MainPage(driver);
+        CategoriesPage categoriesPage = new CategoriesPage(driver);
+        mainPage.clickCategories();
+        categoriesPage.clickAddNewCategoryButton();
+        categoriesPage.clickToEnterNewCategory();
+        categoriesPage.inputNewCategory(categoryName);
+        categoriesPage.clickAddCategory();
+        assertEquals("Category name must start with an uppercase letter, followed by lowercase letters, without numbers, consecutive repeated characters, and a length between 5 and 50 characters", driver.findElement(By.cssSelector("div#liveAlertPlaceholder > div > div")).getText());
     }
 }
